c0211c57526e4ce947e3a9b768139ce494d96a06
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-10-22 02:09:36 -0400

    Bunch of styling done, palette selected

---
+++ app.js
@@ -0,0 +1,13 @@
+// Hamburger menu
+const hamburgerMenu = document.querySelector(".hamburgerMenu")
+const navList = document.querySelector(".navList")
+
+hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active");
+    navList.classList.toggle("active");
+})
+
+document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active")
+    navList.classList.remove("active")
+}))
