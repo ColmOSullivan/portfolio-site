c3b2e2145da5afe8791920dd2c65565645ad821e
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-10-25 00:28:41 -0400

    modal addition, colors solidfied

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", (
     hamburgerMenu.classList.remove("active")
     navList.classList.remove("active")
 }))
+
+// modal button
+var modalBtns = document.querySelectorAll(".modalOpen");
+
+modalBtns.forEach(function(btn) {
+  btn.onclick = function() {
+    var modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "block";
+  };
+});
+
+var closeBtns = document.querySelectorAll('.modalClose');
+
+closeBtns.forEach(function(btn) {
+  btn.onclick = function(){
+    var modal = (btn.closest(".modal").style.display="none");
+  };
+});
+
+window.onclick = function(event){
+    if(event.target.className === 'modal'){
+        event.target.style.display = "none";
+    }
+}
